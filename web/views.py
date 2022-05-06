4dd5929295efc6035b21a1e37d62a2935d404d09
Vladislav Tkachenko <webkadiz@gmail.com>
2022-05-07 00:11:22 +0300

    index.html template

--- web/views.py
+++ web/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(req):
-    return HttpResponse('hello world')
+    context = {}
+    return render(req, 'index.html', context)
